ba78f7fa4c561fe7919a74a6f91df6ab186ca49d
Inne <ineprinusantari@gmail.com>
2019-08-13 11:54:56 +0700

    First Commit | ine

---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container>
+    <v-layout
+        text-center
+        wrap
+      >
+        <v-flex xs12>
+          <h1>This is an about page</h1>
+        </v-flex>
+    </v-layout>
+  </v-container>
+</template>
